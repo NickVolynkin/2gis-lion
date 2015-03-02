c01c50e4a9d7a815ba4c854caf88772f99c629ba
Nick Volynkin <nick.volynkin@gmail.com>
2015-03-03 01:34:51 +0600

    add default maven archetype

---
+++ src/main/java/name/nickvolynkin/test2gis/App.java
@@ -0,0 +1,13 @@
+package name.nickvolynkin.test2gis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
