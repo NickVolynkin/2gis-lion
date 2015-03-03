7c03de0e3df5d127c191312f8bfb610420e33080
Nick Volynkin <nick.volynkin@gmail.com>
2015-03-03 15:49:18 +0600

    rework Action structure, add Lion features

--- src/main/java/name/nickvolynkin/test2gis/lion/Action.java
+++ src/main/java/name/nickvolynkin/test2gis/lion/Action.java
@@ -6,56 +6,48 @@ package main.java.name.nickvolynkin.test2gis.lion;
  * @author Nick Volynkin  nick.volynkin@gmail.com
  */
 public enum Action {
-    EAT {
-        public void perform(
-                final Lion lion,
-                final Event event) {
-            System.out.println("The " + lion + " eats the " + event);
-        }
-    },
-    SLEEP {
+    EAT(Constants.EATS_THE),
+    SLEEP(Constants.SLEEPS_NEAR_THE),
+    ESCAPE(Constants.ESCAPES_FROM_THE),
+    WATCH(Constants.WATCHES_THE),
+    GET_HUNGRY(Constants.GETS) {
         @Override
         public void perform(
                 final Lion lion,
-                final Event event) {
-            System.out.println("The " + lion + " sleeps near the " + event);
-        }
-    },
-    ESCAPE {
-        @Override
-        public void perform(
-                final Lion lion,
-                final Event event) {
-            System.out.println("The " + lion + " escapes from the " + event);
-        }
-    },
-    WATCH {
-        @Override
-        public void perform(
-                final Lion lion,
-                final Event event) {
-            System.out.println("The " + lion + " watches the " + event);
-        }
-    },
-    GET_HUNGRY {
-        @Override
-        public void perform(
-                final Lion lion,
-                final Event event) {
+                final Object event) {
+            super.perform(lion, event);
             lion.setFullness(Fullness.HUNGRY);
         }
     },
-    GET_FULL {
+    GET_FULL(Constants.GETS) {
         @Override
         public void perform(
                 final Lion lion,
-                final Event event) {
+                final Object event) {
+            super.perform(lion, event);
             lion.setFullness(Fullness.FULL);
         }
     };
 
+    Action(final String message) {
+        this.message = message;
+    }
 
-    public abstract void perform(
+    public void perform(
             final Lion lion,
-            Event event);
+            final Object event) {
+        String actionMessage = new StringBuilder().append(lion).append(message).append(event).append(".").toString();
+        lion.say(actionMessage);
+    }
+
+    private final String message;
+
+    private static class Constants {
+
+        public static final String EATS_THE = " eats the ";
+        public static final String SLEEPS_NEAR_THE = " sleeps near the ";
+        public static final String ESCAPES_FROM_THE = " escapes from the ";
+        public static final String WATCHES_THE = " watches the ";
+        public static final String GETS = " gets ";
+    }
 }
--- src/main/java/name/nickvolynkin/test2gis/lion/Lion.java
+++ src/main/java/name/nickvolynkin/test2gis/lion/Lion.java
@@ -1,15 +1,71 @@
 package main.java.name.nickvolynkin.test2gis.lion;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.EnumMap;
+
 /**
  * The Lion state machine.
+ *
  * @author Nick Volynkin  nick.volynkin@gmail.com
  */
 public class Lion {
 
-    private Fullness fullness;
 
-    public void react(Event event) {
+    private static EnumMap<Fullness, EnumMap<Event, Reaction>> reactions;
 
+    static {
+        reactions = new EnumMap<Fullness, EnumMap<Event, Reaction>>(Fullness.class);
+
+        for (Fullness fullness : Fullness.values()) {
+            EnumMap<Event, Reaction> reactionEnumMap = new EnumMap<Event, Reaction>(Event.class);
+            reactions.put(fullness, reactionEnumMap);
+        }
+
+        for (Reaction reaction : Reaction.values()) {
+            reactions.get(reaction.initialState).put(reaction.event, reaction);
+        }
+
+    }
+
+    private Fullness fullness = Fullness.FULL;
+
+    private InputStream input = System.in;
+    private PrintStream output = System.out;
+
+    public Lion() {
+    }
+
+    public
+
+    /**
+     * Lion constructor for testing purpose
+     *
+     * @param inputStream
+     * @param printStream
+     */
+    Lion(
+            final InputStream inputStream,
+            final PrintStream printStream) {
+        this();
+        if (null != inputStream) {
+            this.input = inputStream;
+        }
+
+        if (null != printStream) {
+            this.output = printStream;
+        }
+    }
+
+    public void react(final Event event) {
+        Action[] actions = reactions.get(fullness).get(event).actions;
+        for (Action action : actions) {
+            action.perform(this, event);
+        }
+    }
+
+    public void say(final String string) {
+        output.println(string);
     }
 
     public Fullness getFullness() {
@@ -17,11 +73,13 @@ public class Lion {
     }
 
     void setFullness(final Fullness fullness) {
-        System.out.println("The " + this + " gets " + fullness);
         this.fullness = fullness;
     }
 
-    @Override public String toString() {
-        return "Lion";
+    @Override
+    public String toString() {
+        return "The Lion";
     }
+
+
 }
